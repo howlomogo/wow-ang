012cb99a76b0c64f9c810d03369d2c212b2ed1b9
Matt Pilcher <matt.pilcher@akerolabs.com>
2015-11-16 00:23:09 +0000

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,22 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp        = require('gulp');
+var sass        = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
+
+// Default
+gulp.task('default', ['browser-sync', 'sass-watch']);
+
+// Browser Sync
+gulp.task('browser-sync', function() {
+  console.log("Browser sync started");
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+});
+
+// Sass
 gulp.task('sass', function() {
   console.log("Converting SASS to CSS")
   gulp.src('css/*.scss')
